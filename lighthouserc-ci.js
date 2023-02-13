a32752d5bac28ce8d61a98df8bd5ed88a9f7de68
Rubén Peregrina Aroca <peregrinaruben@gmail.com>
2023-02-13 13:32:49 +0100

    Increase minScore

--- lighthouserc-ci.js
+++ lighthouserc-ci.js
@@ -9,15 +9,15 @@ module.exports = {
                 'http://localhost:4200/features'
             ]
         },
-        asserts: {
+        assert: {
             assertions: {
-                'categories:performance': ['error', {minScore: .6}],
-                'categories:accessibility': ['error', {minScore: .6}],
-                'categories:best-practices': ['error', {minScore: .6}],
-                'categories:seo': ['error', {minScore: .6}],
-                'categories:pwa': 'off',
-            }
-        },
+              "categories:performance": ["error", { minScore: .8 }],
+              "categories:accessibility": ["error", { minScore: .8 }],
+              "categories:best-practices": ["error", { minScore: .8 }],
+              "categories:seo": ["error", { minScore: .8 }],
+              "categories:pwa": "off",
+            },
+          },
         upload: {
             target: 'temporary-public-storage',
         }
